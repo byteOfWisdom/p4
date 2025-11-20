3adfb684548b8d83ab1bba5c3e5914fdf46b9130
Lena <lena@fedora.com>
2025-11-20 18:45:30 +0100

    idk some incorrect wavelengths

--- 442-laser/scripts/wavelength.py
+++ 442-laser/scripts/wavelength.py
@@ -1,4 +1,4 @@
-from sys import argv
+from sys import argv, exit
 
 import numpy as np
 import scipy as sp
@@ -7,11 +7,12 @@ from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
 
-def get_grating_const(filename):
+def get_grating(filename):
     file = filename.split("_")
     temp = file[1].split("mm")
-    grating_const = int(temp[0])
-    return grating_const
+    grating = int(temp[0])
+
+    return grating
 
 
 def get_data(filename):
@@ -23,22 +24,34 @@ def get_data(filename):
 
 
 def main():
-    grating_const = get_grating_const(argv[1])
+    grating = get_grating(argv[1])  # in mm!!
     rep, order, distance = get_data(argv[1])
-    if grating_const == 500:
+    if grating == 500:
         wall = 13e-2
-    elif grating_const == 1000:
+    elif grating == 1000:
         wall = 14e-2
     else:
         sys.exit("you didn't measure this file name?!")
+
+    grating_const = 1e-3 / grating
+    print("g:", grating_const, "m")
     dist_err = 5e-2
 
-    angle = distance / wall  # this feels like the issue
+    angle = distance / 2 * wall  # this feels like the issue
     angle_term = np.sin(angle)
 
     wavelength = (grating_const * angle_term) / order
     for i in range(len(wavelength)):
-        print(wavelength[i], "nm")
+        print(
+            "messung:",
+            int(rep[i]),
+            "order:",
+            int(order[i]),
+            "lambda:",
+            round(wavelength[i] * 1e9, 3),
+            "nm",
+        )
+        # i mean they're at least consistent between messungen??? idk anymore
 
 
 if __name__ == "__main__":
