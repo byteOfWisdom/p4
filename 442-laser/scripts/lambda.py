471bfe2c5b71c15d25211064de2f1f67fd2d23f1
Leoni <leoni.dessau@mailbox.org>
2025-11-22 18:12:54 +0100

    lambda works for 500 per mm grating

---
+++ 442-laser/scripts/lambda.py
@@ -0,0 +1,40 @@
+#!python3
+import scipy
+import numpy as np
+from matplotlib import pyplot as plt
+import std
+import propeller as p
+from sys import argv
+
+# std.bullshit.this_is_fucking_stupid_no_one_actually_gives_a_fuck()
+
+
+def main():
+    data = np.transpose(np.loadtxt(argv[1], delimiter=",", skiprows=1))
+    # measure_num = data[0][:3]
+    order = data[1]
+    order -= 1
+    translation = p.ev(data[2], 5e-3)
+    translation = translation[0] - translation
+
+    distance = float(argv[2])
+    grating_const = float(argv[3])
+    angle_term = np.sin(np.arctan(translation / distance))
+    angle_term_v, angle_term_e = p.ve(grating_const * angle_term)
+    fmt = std.default.error_bar_def
+    fmt["fmt"] = "x"
+    plt.errorbar(order, angle_term_v, yerr=angle_term_e, **fmt)
+    std.default.plt_pretty("Ordnung", "Winkelterm")
+
+    res, meta = std.fit_func(lambda x, a: a * x, order, angle_term_v, y_errors=angle_term_e)
+
+    x = np.linspace(0, 2, 100)
+    plt.plot(x, x * res[0])
+
+    print(f"{res[0]} +- {meta.sd_beta[0]}")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
