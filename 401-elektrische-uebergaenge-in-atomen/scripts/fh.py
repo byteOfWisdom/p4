41abc7404d11724ba37b46ed6f3836662f945184
Leoni <leoni.dessau@mailbox.org>
2025-12-15 17:30:22 +0100

    more data

--- 401-elektrische-uebergaenge-in-atomen/scripts/fh.py
+++ 401-elektrische-uebergaenge-in-atomen/scripts/fh.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot as plt
 import scipy
 import std
 
+peaknum = 7
+
 
 def load(file):
     data = np.transpose(np.loadtxt(file, delimiter="\t", skiprows=5))
@@ -12,33 +14,24 @@ def load(file):
 
 
 def penta_gauss(x, *args):
-    args = iter(args)
-    g1 = std.gaussian(x, next(args), next(args), next(args))
-    g2 = std.gaussian(x, next(args), next(args), next(args))
-    g3 = std.gaussian(x, next(args), next(args), next(args))
-    g4 = std.gaussian(x, next(args), next(args), next(args))
-    g5 = std.gaussian(x, next(args), next(args), next(args))
-    # g6 = std.gaussian(x, next(args), next(args), next(args))
-    return g1 + g2 + g3 + g4 + g5 #+ g6
+    return sum([std.gaussian(x, args[i], args[i + 1], args[i + 2]) for i in range(0, 3 * peaknum, 3)])
 
 
 def main():
     U_acc, U_I = load(argv[1])
-    peaks = U_acc[scipy.signal.find_peaks(U_I, width=5, prominence=0.0005)[0]]
-
-    # peaks = np.append([peaks[0] - 5], peaks)
-    n_peaks = len(peaks)
-    peak_iter = iter(peaks)
-    p0 = np.zeros(n_peaks * 3 + 3)
-    for i in range(0, n_peaks * 3, 3):
-        p0[i] = i
-        p0[i + 1] = next(peak_iter)
-        p0[i + 2] = 1.5
-    p0[-3] = 6
-    p0[-2] = peaks[-1] + 5
-    p0[-1] = 1.5
-
+    # peaks = U_acc[scipy.signal.find_peaks(U_I, width=5, prominence=0.0005)[0]]
+    peaks = np.linspace(7, 36, peaknum)
     print(peaks)
+    amps = np.linspace(1, 7, peaknum)
+    sigmas = np.ones(peaknum) * 1.5
+
+    p0 = np.zeros(3 * peaknum)
+
+    for i in range(0, peaknum):
+        p0[3 * i] = amps[i]
+        p0[3 * i + 1] = peaks[i]
+        p0[3 * i + 2] = sigmas[i]
+
     params, cov = scipy.optimize.curve_fit(penta_gauss, U_acc, U_I, p0=p0, maxfev=99999)
     eb_param = std.default.error_bar_def
 
