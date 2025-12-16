cee6ec9519e283fa2d83b42aa949be7fc8c74f68
Leoni <leoni.dessau@mailbox.org>
2025-12-17 00:11:57 +0100

    sexy sexy data analysis

--- 401-elektrische-uebergaenge-in-atomen/scripts/fh.py
+++ 401-elektrische-uebergaenge-in-atomen/scripts/fh.py
@@ -5,39 +5,85 @@ from matplotlib import pyplot as plt
 import scipy
 import std
 
-peaknum = 7
-
 
 def load(file):
     data = np.transpose(np.loadtxt(file, delimiter="\t", skiprows=5))
     return data[2], data[1]
 
 
-def penta_gauss(x, *args):
-    return sum([std.gaussian(x, args[i], args[i + 1], args[i + 2]) for i in range(0, 3 * peaknum, 3)])
+def make_n_gaussian(n):
+    return lambda x, *args: sum([std.gaussian(x, args[i], args[i + 1], args[i + 2]) for i in range(0, 3 * n, 3)])
+
+
+def fit_multi_gauss(x, y):
+    peak_ids, _ = scipy.signal.find_peaks(y, width=5, prominence=0.01, distance=20)
+    print(peak_ids)
+    peak_x = x[peak_ids]
+    print(peak_x)
+
+    delta_x = np.abs(0.4 * np.average(peak_x[1:] - peak_x[:-1]))
+
+    a, µ, sigma = [], [], []
+
+    for i in range(len(peak_ids)):
+        mask = np.abs(x - peak_x[i]) < delta_x
+        x_slice = x[mask]
+        y_slice = y[mask]
+
+        p0=[max(y_slice), x_slice[y_slice == max(y_slice)][0], 0.1 * (x_slice[-1] - x_slice[0])]
+
+        if p0[1] == x_slice[-1]:
+            p0[1] = µ[-1] + (µ[-1] - µ[-2])
+
+        params, _ = std.fit_func(std.gaussian, x_slice, y_slice, p0=p0)
+
+        a.append(params[0])
+        µ.append(params[1])
+        sigma.append(params[2])
+
+        # plt.scatter(x_slice, y_slice, marker="x")
+        # plt.plot(x_slice, std.gaussian(x_slice, *params))
+        # std.default.plt_pretty("x", "y")
+        # plt.show()
+
+    p0 = []
+    for i in range(len(peak_ids)):
+        p0.append(a[i])
+        p0.append(µ[i])
+        p0.append(sigma[i])
+
+    f = make_n_gaussian(len(peak_ids))
+    params, meta = std.fit_func(f, x, y, p0=p0)
+
+    return params
 
 
 def main():
+    global µ_bound
     U_acc, U_I = load(argv[1])
-    # peaks = U_acc[scipy.signal.find_peaks(U_I, width=5, prominence=0.0005)[0]]
-    peaks = np.linspace(7, 36, peaknum)
-    print(peaks)
-    amps = np.linspace(1, 7, peaknum)
-    sigmas = np.ones(peaknum) * 1.5
 
-    p0 = np.zeros(3 * peaknum)
+    params = fit_multi_gauss(U_acc, U_I)
+    µ = params[1::3]
+    sort_key = np.argsort(µ)
+    sigma = params[2::3][sort_key]
+    amp = params[0::3][sort_key]
+    µ = µ[sort_key]
 
-    for i in range(0, peaknum):
-        p0[3 * i] = amps[i]
-        p0[3 * i + 1] = peaks[i]
-        p0[3 * i + 2] = sigmas[i]
+    delta_µ = µ[1:] - µ[:-1]
 
-    params, cov = scipy.optimize.curve_fit(penta_gauss, U_acc, U_I, p0=p0, maxfev=99999)
+    print(f"fittet µ are : {np.vectorize(lambda x: round(x, 2))(µ)}")
+    print(f"delta µ is : {delta_µ}")
+    print(f"average delta µ is : {np.sum(delta_µ) / len(delta_µ)}")
+
     eb_param = std.default.error_bar_def
-
     plt.errorbar(U_acc, U_I, xerr=0.1, yerr=0.1, **eb_param)
+
+    multi_gauss = make_n_gaussian(len(µ))
     xrange = np.linspace(min(U_acc), max(U_acc), 10000)
-    plt.plot(xrange, [penta_gauss(x, *params) for x in xrange])
+    plt.plot(xrange, [multi_gauss(x, *params) for x in xrange])
+    for i in range(len(µ)):
+        plt.plot(xrange, std.gaussian(xrange,  amp[i], µ[i], sigma[i]), linestyle="dashdot")
+
     std.default.plt_pretty("Beschleunigungsspannung / V", "Strom / Einheit")
     plt.show()
 
