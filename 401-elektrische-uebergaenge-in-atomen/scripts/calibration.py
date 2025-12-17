9ec388ab65a11af7dd20082958507d60603b8322
Lena <lena@fedora.com>
2025-12-17 13:42:31 +0100

    field calibration curves

--- 401-elektrische-uebergaenge-in-atomen/scripts/calibration.py
+++ 401-elektrische-uebergaenge-in-atomen/scripts/calibration.py
@@ -6,9 +6,6 @@ import scipy
 import std
 from matplotlib import pyplot as plt
 
-std.bullshit.ger()
-
-
 def get_curve(file):
     data = np.transpose(np.loadtxt(file, delimiter=";", skiprows=2))
     # convert to SI units
@@ -17,14 +14,21 @@ def get_curve(file):
     return current, field
 
 
-def main():
-    std.default.plt_pretty("Spulenstrom / A", "Magnetfeld B / T")
+def calibration_curve(to_print=False,to_plot=True):
+    std.bullshit.ger()
+    std.default.plt_pretty("Spulenstrom I / A", "Magnetfeld B / T")
     cubic = lambda x, a, b, c, d: (a * (x**3)) + (b * (x**2)) + (c * x) + d
-    for i in range(len(argv)):
-        if i >= 1:
-            currents, fields = get_curve(argv[i])
-            params, cov = scipy.optimize.curve_fit(cubic, ~currents, ~fields)
-            errs = np.sqrt(np.diag(cov))
+    plot_color = iter(["crimson","forestgreen"])
+    func_params = []
+    func_errs = []
+    for i in range(1,3):
+        this_color = next(plot_color)
+        currents, fields = get_curve(argv[i])
+        params, cov = scipy.optimize.curve_fit(cubic, ~currents, ~fields)
+        errs = np.sqrt(np.diag(cov))
+        func_params.append(params)
+        func_errs.append(errs)
+        if to_print is True:
             print(
                 "a:",
                 params[0],
@@ -43,13 +47,22 @@ def main():
                 "+-",
                 errs[3],
             )
-
-            eb_param = std.default.error_bar_def
-            _, curr_err = p.ve(currents)
-            _, field_err = p.ve(fields)
-            goodness = round(std.goodness_of_fit(~fields, cubic(~currents, *params)), 4)
+        eb_param = std.default.error_bar_def
+        _, curr_err = p.ve(currents)
+        _, field_err = p.ve(fields)
+        goodness = round(std.goodness_of_fit(~fields, cubic(~currents, *params)), 4)
+        if to_print is True:
             print("R^2,meas:", goodness)
             # errors are placeholders
+        if to_plot is True:
+            xrange = np.linspace(min(~currents), max(~currents), 1000)
+            plt.plot(
+                xrange,
+                [cubic(x, *params) for x in xrange],
+                label=rf"$R^2_{i}$={goodness}",
+                color = this_color,
+                linestyle = "--"
+            )
             plt.errorbar(
                 ~currents,
                 ~fields,
@@ -57,38 +70,47 @@ def main():
                 yerr=field_err,
                 **eb_param,
                 label=f"Messung {i}",
+                color = this_color,
             )
-            xrange = np.linspace(min(~currents), max(~currents), 1000)
-            plt.plot(
-                xrange,
-                [cubic(x, *params) for x in xrange],
-                label=rf"Fit {i}, $R^2$={goodness}",
-            )
-        else:
-            continue
-        if i == 1:
-            params_1 = params
-        if i == 2:
-            params_2 = params
+            plt.legend(loc="best")
+        # if i == 1:
+        #     params_1 = params
+        # if i == 2:
+        #     params_2 = params
+    return func_params,func_errs
 
     # just to evaluate differences, dont include in final graph
-    diff_xrange = np.linspace(-10, 10, 1000)
-    cubic_1 = [cubic(x, *params_1) for x in diff_xrange]
-    cubic_2 = [cubic(x, *params_2) for x in diff_xrange]
-    diff_cubic = np.linspace(0, 1, len(diff_xrange))
-    for x in range(len(diff_xrange)):
-        diff_cubic[x] = cubic_1[x] - cubic_2[x]
-    plt.plot(diff_xrange, diff_cubic, label="Differenz der Fitfunktionen")
+    # diff_xrange = np.linspace(-10, 10, 1000)
+    # cubic_1 = [cubic(x, *params_1) for x in diff_xrange]
+    # cubic_2 = [cubic(x, *params_2) for x in diff_xrange]
+    # diff_cubic = np.linspace(0, 1, len(diff_xrange))
+    # for x in range(len(diff_xrange)):
+    #     diff_cubic[x] = cubic_1[x] - cubic_2[x]
+    #plt.plot(diff_xrange, diff_cubic, label="Differenz der Fitfunktionen")
+def average_field():
+    params,errs = calibration_curve(to_plot=False)
+    params_1 = p.ev(params[0],errs[0])
+    params_2 = p.ev(params[1],errs[1])
+    p_1, _ = p.ve(params_1)
+    p_2, _ = p.ve(params_2)
+    print("Parameterset 1:", p_1)
+    print("Parameterset 2:", p_2)
+    av_params = (params_1+params_2) / 2
+    av_vals, av_errs = p.ve(av_params)
+    cubic = lambda x: (av_params[0] * (x**3)) + (av_params[1] * (x**2)) + (av_params[2] * x) + av_params[3]
+    temp,_ = p.ve(np.vectorize(cubic)(np.linspace(-10, 10, 100)))
+   # plt.plot(np.linspace(-10, 10, 100), temp, color="black")
+    return cubic
 
-    plt.legend(loc="best")
-    plt.show()
 
 
-# def fit_cubic(file):
-#     currents, fields = get_curve(file)
-#     cubic = lambda x, a, b, c, d: (a * (x**3)) + (b * (x**2)) + (c * x) + d
-#     params, cov = scipy.optimize.curve_fit(cubic, currents, fields)
-#     return params, cov
+def main():
+    calibration_curve()
+    average_field()
+    if len(argv) >= 4:
+        plt.savefig(argv[3])
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
