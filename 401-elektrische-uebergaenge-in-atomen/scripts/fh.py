7aeebbad831b17df1122e44199d6b62cb39a4f35
Leoni <leoni.dessau@mailbox.org>
2025-12-17 16:30:07 +0100

    asdf

--- 401-elektrische-uebergaenge-in-atomen/scripts/fh.py
+++ 401-elektrische-uebergaenge-in-atomen/scripts/fh.py
@@ -4,6 +4,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import scipy
 import std
+from dataclasses import dataclass
+from uncertainties import ufloat
 
 
 def load(file):
@@ -70,14 +72,24 @@ def fit_multi_gauss(x, y):
     return params, std_devs, r_sq
 
 
+@dataclass
+class fit_res:
+    µ: np.ndarray
+    err_µ: np.ndarray
+    sigma: np.ndarray
+    err_sigma: np.ndarray
+    amp: np.ndarray
+    err_amp: np.ndarray
+
+
 def process_file(file, save=False, show=False):
     U_acc, U_I = load(file)
 
     params, std_devs, r_sq = fit_multi_gauss(U_acc, U_I)
-    µ = params[1::3]
+    µ, err_µ = params[1::3], std_devs[1::3]
     sort_key = np.argsort(µ)
-    sigma = params[2::3][sort_key]
-    amp = params[0::3][sort_key]
+    sigma, err_sigma = params[2::3][sort_key], std_devs[2::3][sort_key]
+    amp, err_amp = params[0::3][sort_key], std_devs[0::3][sort_key]
     µ = µ[sort_key]
 
     if show or save:
@@ -99,7 +111,11 @@ def process_file(file, save=False, show=False):
         plt.legend()
         plt.show()
 
-    return µ
+    return fit_res(µ, err_µ, sigma, err_sigma, amp, err_amp)
+
+
+def write_table(data, to_file):
+    pass
 
 
 def main():
@@ -118,7 +134,6 @@ def main():
     elif "-" in argv[2]:
         id = range(int(argv[2].split("-")[0]), int(argv[2].split("-")[1]) + 1)
 
-
     filter = np.ones(len(temps))
     if len(argv) > 3:
         T = np.array(temps)
@@ -127,21 +142,24 @@ def main():
         _ = T
         _ = U
 
-    peak_pos, peak_num = [], []
-    progress = std.pbar(len(id), msg="running fits ")
-    progress.next()
+    peak_pos, peak_num, all_res = [], [], []
+    if not save:
+        progress = std.pbar(len(id), msg="running fits ")
+        progress.next()
 
     for i in id:
-        progress.next()
+        if not save:
+            progress.next()
         if not filter[i]:
             continue
         # print(f"running for: T = {temps[i]}C; U_max = {u_max[i]}V; U_2 = {u_2[i]}V")
-        µ = process_file(file[i], save)[:-1]
-        µ = µ # - µ[0]
+        res = process_file(file[i], save)
+        µ = res.µ[:-1]
         peak_ids = np.array(range(len(µ)))
         peak_ids += 0 if µ[0] < 10 else 1
         peak_pos = np.append(peak_pos, µ)
         peak_num = np.append(peak_num, peak_ids)
+        all_res.append(res)
 
     peak_pos = peak_pos[peak_num > 0]
     peak_num = peak_num[peak_num > 0]
@@ -151,7 +169,7 @@ def main():
     plt.scatter(peak_num, peak_pos, marker="x")
     xrange = np.linspace(0, max(peak_num) + 0.5)
     plt.plot(xrange, (lambda x, a, b: a * x + b)(xrange, *params), label=f"$R^2 = {r_sq}$")
-    std.default.plt_pretty("Nummer", "Position")
+    std.default.plt_pretty("Nummer des Maximums", "$U_B$ / V")
     plt.show()
 
 
