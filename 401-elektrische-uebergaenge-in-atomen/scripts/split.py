d0d82cdf7f128bceb2061d0b650f4c379bad6817
Leoni <leoni.dessau@mailbox.org>
2025-12-21 16:43:54 +0100

    asdf

--- 401-elektrische-uebergaenge-in-atomen/scripts/split.py
+++ 401-elektrische-uebergaenge-in-atomen/scripts/split.py
@@ -44,7 +44,32 @@ def isolate_orders(x, y):
  
 
 def make_n_gaussian(n):
-    return lambda x, *args: args[-1] + sum([std.gaussian(x, np.abs(args[i]), args[i + 1], args[i + 2]) for i in range(0, 3 * n, 3)])
+    return lambda x, *args: args[-1] + sum([std.gaussian(x, args[i], args[i + 1], args[i + 2]) for i in range(0, 3 * n, 3)])
+
+
+def diff_find_maxima(y, smoothing=2):
+    # smooth_grad = np.roll(0.2 * np.convolve(np.gradient(y), np.ones(2 * smoothing), mode="same"), 0)
+    smooth_grad = np.gradient(np.convolve(y, np.ones(2 * smoothing), mode="same"))
+    # plt.cla()
+    # plt.plot(np.gradient(y))
+    # plt.plot(smooth_grad)
+    # plt.plot(y)
+    # plt.show()
+
+    peaks = []
+
+    before, after = np.zeros(len(y), dtype=np.bool), np.zeros(len(y), dtype=np.bool)
+    for i in range(smoothing):
+        before[i] = 1
+        after[i + smoothing] = 1
+
+    for i in range(smoothing, len(y) - smoothing):
+        if np.all(smooth_grad[before] > 0) and np.all(smooth_grad[after] < 0):
+            peaks.append(i - 1)
+        before = np.roll(before, 1)
+        after = np.roll(after, 1)
+
+    return peaks
 
 
 @dataclass
@@ -53,18 +78,16 @@ class peak_descriptor:
     sigma: ufloat
     fwhm: ufloat
     position: ufloat
+    valid: bool
 
 
 def fit_order(x, y):
-    # plt.cla()
-    # plt.plot(x, y)
-    # plt.show()
-    kernel = np.ones(int(len(x) / 10))
-    peaks, _ = scipy.signal.find_peaks(scipy.signal.convolve(y, kernel), width=5, height=(min(y) + 0.3 * (max(y) - min(y))))
-    peaks -= len(kernel)
+    peaks = diff_find_maxima(y, smoothing=len(y) // 25)
+    # peaks -= len(kernel)
     print(len(peaks), x[peaks])
+    
     func = make_n_gaussian(len(peaks))
-    print([y[max(0, peak-5):peak+5] for peak in peaks])
+    # print([y[max(0, peak - 5):peak+5] for peak in peaks])
     amp_initial = [max(y[max(0, peak - 5):peak + 5]) for peak in peaks]
     µ_initial = [(x[max(0, peak - 5):peak + 5])[y[max(0, peak - 5):peak + 5] == max(y[max(0, peak - 5):peak + 5])][0] for peak in peaks]
     sigma_initial = (max(x) - min(x)) / 10
@@ -79,18 +102,27 @@ def fit_order(x, y):
 
     res = []
     for i in range(len(peaks)):
+        valid = (params[3 * i] + params[-1] < 1.1 * max(y)) and (min(x) < params[3 * i + 1] < max(x))
         peak = peak_descriptor(
                                height=ufloat(params[3 * i], errors[3 * i]) + ufloat(params[-1], errors[-1]),
                                sigma=ufloat(params[3 * i + 2], errors[3 * i + 2]),
                                fwhm=ufloat(params[3 * i + 2], errors[3 * i + 2]) * 2.355,
-                               position=ufloat(params[3 * i + 1], errors[3 * i + 1]))
-        res.append(peak)
+                               position=ufloat(params[3 * i + 1], errors[3 * i + 1]),
+                               valid=valid)
+        if peak.valid:
+            res.append(peak)
+        else:
+            print("rececting implausible fit")
 
     # _ = [print(r) for r in res]
+    # print(params)
     # plt.cla()
     # plt.plot(x, y)
     # xrange = np.linspace(min(x), max(x), 1000)
     # plt.plot(xrange, func(xrange, *params))
+    # plt.plot(xrange, std.gaussian(xrange, *params[0:3]) + params[-1], linestyle="dotted")
+    # plt.plot(xrange, std.gaussian(xrange, *params[3:6]) + params[-1], linestyle="dotted")
+    # plt.plot(xrange, std.gaussian(xrange, *params[6:9]) + params[-1], linestyle="dotted")
     # plt.scatter([p.position.nominal_value for p in res], [p.height.nominal_value for p in res], marker="x")
     # plt.show()
 
