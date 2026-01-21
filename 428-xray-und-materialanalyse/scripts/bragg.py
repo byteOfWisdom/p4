afa9e471aceb1871b80889fedbd25c064b7744f5
Lena <lena@email.com>
2026-01-21 16:07:12 +0100

    now even more broken

--- 428-xray-und-materialanalyse/scripts/bragg.py
+++ 428-xray-und-materialanalyse/scripts/bragg.py
@@ -38,6 +38,7 @@ def convert(file: str, order: int = 1):
     #     color="hotpink",
     #     linewidth=1.2,
     # )
+    #
     std.default.plt_pretty(f"Energie [keV]", "Intensität")
     plt.plot(
         energies_ev * 1e-3,
@@ -51,44 +52,84 @@ def convert(file: str, order: int = 1):
 
 
 def gaussian(x, a, mu, sigma):
-    return a * np.exp(((x - mu) / sigma) ** 2)
+    return a * np.exp(-(((x - mu) / sigma) ** 2))
 
 
-def linear_background(x, a, b):
-    return a * x + b
+def background(x, a, b):
+    return -a * ((x - 15) ** 2) + b
 
 
-def spectrum_func(n: int):
+def spectrum_func(n):
     multigaussian = lambda x, *params: sum(
         [
             gaussian(x, params[i], params[i + 1], params[i + 2])
             for i in range(0, 3 * n, 3)
         ]
-    ) + linear_background(x, params[3 * n + 3], params[3 * n + 4])
+    ) + background(x, params[3 * n], params[3 * n + 1])
     return multigaussian
+
+
+def fitted_spectrum(x, n, *params):
+    return sum(
+        [
+            gaussian(x, params[i], params[i + 1], params[i + 2])
+            for i in range(0, 3 * n, 3)
+        ]
+    ) + background(x, params[3 * n], params[3 * n + 1])
 
 
 def fit_peaks(file: str):
     init_guess = []  # for gaussian params
 
-    linear_fit = [160, -50]
-    gauss_easy = [1, 1, 1]
-    init_guess = (gauss_easy * 6) + linear_fit
+    xrange = np.linspace(0, 50, 500)
+    linear_fit = [8, 1000]
+    gauss0 = [100, 5.5, 0.06]
+    gauss1 = [100, 7, 0.06]
+    gauss2 = [600, 8, 0.1]
+    gauss3 = [1000, 9.5, 0.1]
+    gauss4 = [800, 12, 0.15]
+
+    manual = (
+        gaussian(xrange, *gauss0)
+        + gaussian(xrange, *gauss1)
+        + gaussian(xrange, *gauss2)
+        + gaussian(xrange, *gauss3)
+        + gaussian(xrange, *gauss4)
+        + background(xrange, *linear_fit)
+    )
+    plt.plot(xrange, manual)
+    energies_ev, counts = convert(file)
+
+    init_guess = gauss0 + gauss1 + gauss2 + gauss3 + gauss4 + linear_fit
     print(init_guess)
 
     energies_ev, counts = convert(file)
     # TO DO: data slicing
-    #
+    energies_range = []
+    counts_range = []
+    for i in range(len(energies_ev)):
+        if energies_ev[i] <= 35e3:
+            energies_range.append(energies_ev[i])
+            counts_range.append(counts[i])
+    plt.plot(energies_range, counts_range)
     fit, cov = scipy.optimize.curve_fit(
-        spectrum_func(6), energies_ev, counts, p0=init_guess
+        spectrum_func(5),
+        energies_ev,
+        counts,
+        p0=init_guess,
+        maxfev=9999,
     )
-
-    # fit parameter error here -> issue with fitting func
+    # fit parameter error here
 
     err = np.sqrt(np.diag(cov))
-    for i in fit:
-        print(fit[i])
+    print(fit)
 
+    fit_range = np.linspace(0, 35e3, len(energies_range))
+    fitted_func = fitted_spectrum(fit_range, 5, init_guess)
+
+    plt.plot(fit_range, fitted_spectrum(fit_range, 5, *init_guess), label="fitted")
+    plt.legend()
+    plt.show()
     return
 
 
